e86270419f40a13de1518859cbdecf344b05649b
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-10-21 02:13:47 +0300

    .

--- src/test/java/training01/ex001/test_ex001.py
+++ src/test/java/training01/ex001/test_ex001.py
@@ -1,3 +1,3 @@
 def test_ex001():
-    from src.main.java.training01.ex001.ex001 import reverseArray
+    from src.main.java.training01.ex001.code import reverseArray
     assert [3, 2, 1] == reverseArray([1, 2, 3])
