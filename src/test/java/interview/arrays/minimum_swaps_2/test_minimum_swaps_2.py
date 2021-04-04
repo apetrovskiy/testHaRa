71ce29a7a66d4a5a7b4827868294459e25c2797c
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-05 00:34:03 +0300

    preparation

---
+++ src/test/java/interview/arrays/minimum_swaps_2/test_minimum_swaps_2.py
@@ -0,0 +1,12 @@
+import pytest
+from typing import List
+from src.main.java.interview.arrays.minimum_swaps_2.minimum_swaps_2 import minimumSwaps
+
+
+test_data = [([4, 3, 1, 2], 3), ([2, 3, 4, 1, 5], 3),
+             ([1, 3, 5, 2, 4, 6, 7], 3)]
+
+
+@pytest.mark.parametrize("input_array,expected_result", test_data)
+def test_minimum_swaps_2(input_result: List[int], expected_result: int):
+    assert expected_result == minimumSwaps(input_result)
