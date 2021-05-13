8ba143515cc13b6186333341d42517fb10e9a77c
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-13 16:03:49 +0300

    Update greedy_florist.test.js

--- src/main/java/interview/greedy/GreedyFlorist/greedy_florist.test.js
+++ src/main/java/interview/greedy/GreedyFlorist/greedy_florist.test.js
@@ -1,12 +1,11 @@
 const getMinimumCost = require("./greedy_florist").getMinimumCost;
-// const { assert, expect } = require("chai");
 
 describe.each([
-        [3, [2, 5, 6], 13],
-        [2, [2, 5, 6], 15],
-        [3, [1, 3, 5, 7, 9], 29],
-])('getMinimumCost(%i, %o) should equal %i', (k, c, expectedResult) => {
-    test(`people = ${k}, cost = ${c}, returns ${expectedResult}`, () => {
-        expect(getMinimumCost(k, c)).toBe(expectedResult);
-    });
+  [3, [2, 5, 6], 13],
+  [2, [2, 5, 6], 15],
+  [3, [1, 3, 5, 7, 9], 29],
+])("getMinimumCost(%i, %o) should equal %i", (k, c, expectedResult) => {
+  test(`people = ${k}, cost = ${c}, returns ${expectedResult}`, () => {
+    expect(getMinimumCost(k, c)).toBe(expectedResult);
+  });
 });
