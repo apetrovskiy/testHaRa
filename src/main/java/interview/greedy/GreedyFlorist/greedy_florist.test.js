67f557aef94393ac03b54cd66d565c407db8a552
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-13 14:43:13 +0300

    .

---
+++ src/main/java/interview/greedy/GreedyFlorist/greedy_florist.test.js
@@ -0,0 +1,13 @@
+import each from 'jest-each';
+const getMinimumCost = require("./greedy_florist").getMinimumCost;
+const { assert, expect } = require("chai")
+
+describe('getMinimumCost(k, c)', () => {
+    each([
+        [3, [2, 5, 6], 13],
+        [2, [2, 5, 6], 15],
+        [3, [1, 3, 5, 7, 9], 29],
+    ]).test(`people = ${k}, cost = ${c}`, (k, c, expectedResult) => {
+        expect(getMinimumCost(k, c).toBe(expectedResult));
+    });
+});
