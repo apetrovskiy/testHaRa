0c658a3d3be381f0be173699d00daeada5032487
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-13 17:23:16 +0300

    Update greedy_florist.test.js

--- src/main/java/interview/greedy/GreedyFlorist/greedy_florist.test.js
+++ src/main/java/interview/greedy/GreedyFlorist/greedy_florist.test.js
@@ -4,7 +4,7 @@ describe.each([
     [3, [2, 5, 6], 13],
     [2, [2, 5, 6], 15],
     [3, [1, 3, 5, 7, 9], 29],
-])("getMinimumCost(%i, %o) should equal %i", (k, c, expectedResult) => {
+])('getMinimumCost(%i, %o) should equal %i', (k, c, expectedResult) => {
     test(`people = ${k}, cost = ${c}, returns ${expectedResult}`, () => {
         expect(getMinimumCost(k, c)).toBe(expectedResult);
     });
